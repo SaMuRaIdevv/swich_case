1cf33370a78af6ad4d94fe835164cb0385edde5c
SaMuRaIdevv <mukhridd1nchoriyev2007@gmail.com>
2024-03-28 16:59:58 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,561 @@
+"use strict"
+
+// CASE-1
+
+// let numberOfWeek = 3;
+
+// if(numberOfWeek <= 7){
+//     switch(numberOfWeek){
+//         case 1:
+//             console.log("Monday");
+//             break; 
+//         case 2:
+//             console.log("Tuesday"); 
+//             break;
+//         case 3:
+//             console.log("Wednesday"); 
+//             break;
+//         case 4:
+//             console.log("Thursday");
+//             break; 
+//         case 5:
+//             console.log("Friday"); 
+//             break;
+//         case 6:
+//             console.log("Saturday");
+//             break; 
+//         case 7:
+//             console.log("Sunday"); 
+//             break;
+//     }
+// }else{
+//     console.log(Error);
+// }
+
+// CASE-2
+
+// let k = 4
+
+// if(k <= 5) {
+//     switch(k){
+//         case 1:
+//             console.log("yomon!");
+//             break;
+//         case 2:
+//             console.log("qoniqarsiz");
+//             break;
+//         case 3:
+//             console.log("qoniqarli");
+//             break;
+//         case 4:
+//             console.log("yaxshi");
+//             break;
+//         case 5:
+//             console.log("A`lo");
+//             break;
+//     }
+// }else if(k <= 0) {
+//     console.log("Musbat son kiriting");
+// }else{
+//     console.log("5 dan katta son kiritilmasin");
+// }
+
+// CASE-3
+
+// let numberOfMonth = 5
+
+// if(numberOfMonth <= 12){
+//     switch(numberOfMonth){
+//         case 1:
+//             console.log("qish");
+//             break;
+//         case 2:
+//             console.log("qish");
+//             break;
+//         case 3:
+//             console.log("bahor");
+//             break;
+//         case 4:
+//             console.log("bahor");
+//             break;
+//         case 5:
+//             console.log("bahor");
+//             break;
+//         case 6:
+//             console.log("yoz");
+//             break;
+//         case 7:
+//             console.log("yoz");
+//             break;
+//         case 8:
+//             console.log("yoz");
+//             break;
+//         case 9:
+//             console.log("kuz");
+//             break;
+//         case 10:
+//             console.log("kuz");
+//             break;
+//         case 11:
+//             console.log("kuz");
+//             break;
+//         case 12:
+//             console.log("qish");
+//             break;
+//     }
+// }else{
+//     console.log(Error);
+// }
+
+// CASE-4
+
+// let numberOfMonth = 5
+
+// if(numberOfMonth <= 12){
+//     switch(numberOfMonth){
+//         case 1:
+//             console.log("Yanvar oyida 31 kun bor");
+//             break;
+//         case 2:
+//             console.log("Fevral oyida 29 kun bor");
+//             break;
+//         case 3:
+//             console.log("Mart oyida 31 kun bor");
+//             break;
+//         case 4:
+//             console.log("Aprel oyida 30 kun bor");
+//             break;
+//         case 5:
+//             console.log("May oyida 31 kun bor");
+//             break;
+//         case 6:
+//             console.log("Iyun oyida 30 kun bor");
+//             break;
+//         case 7:
+//             console.log("Iyul oyida 31 kun bor");
+//             break;
+//         case 8:
+//             console.log("Avgust oyida 31 kun bor");
+//             break;
+//         case 9:
+//             console.log("Sentabr oyida 30 kun bor");
+//             break;
+//         case 10:
+//             console.log("Oktabr oyida 31 kun bor");
+//             break;
+//         case 11:
+//             console.log("Noyabr oyida 30 kun bor");
+//             break;
+//         case 12:
+//             console.log("Dekabr oyida 31 kun bor");
+//             break;
+//     }
+// }else{
+//     console.log(Error);
+// }
+
+// CASE-5
+
+// let a = 4, b = 5, arifmetic = "bo`lish"
+
+// switch(arifmetic){
+//     case "qo`shish":
+//         console.log(a+b);
+//         break;
+//     case "ayirish":
+//         console.log(a-b);
+//         break;
+//     case "ko`paytirish":
+//         console.log(a*b);
+//         break;
+//     case "bo`lish":
+//         console.log(a/b);
+//         break;
+// }
+
+// CASE-6
+
+// let birlik = 5, uzunlik = 5
+
+// if(birlik <=5 ){
+//     switch(birlik){
+//         case 1:
+//             console.log(uzunlik*0.1,"metr");
+//             break;
+//         case 2:
+//             console.log(uzunlik*1000,"metr");
+//             break;
+//         case 3:
+//             console.log(uzunlik,"metr");
+//             break;
+//         case 4:
+//             console.log(uzunlik*0.001,"metr");
+//             break;
+//         case 5:
+//             console.log(uzunlik*0.01,"metr");
+//             break;
+//     }
+// }else{
+//     console.log(Error);
+// }
+
+// CASE-7
+
+// let birlik = 4, massa = 25
+
+// if(birlik <=5 ){
+//     switch(birlik){
+//         case 1:
+//             console.log(massa,"kilogram");
+//             break;
+//         case 2:
+//             console.log(massa*0.000001,"kilogram");
+//             break;
+//         case 3:
+//             console.log(massa*0.001,"kilogram");
+//             break;
+//         case 4:
+//             console.log(massa*1000,"kilogram");
+//             break;
+//         case 5:
+//             console.log(massa*100,"kilogram");
+//             break;
+//     }
+// }else{
+//     console.log(Error);
+// }
+
+// CASE-8
+
+// let day = 2, month = 11
+
+// if(0 < day && 28>=day, 0 < month && 12>=month){
+//     switch(month){
+//         case 1:
+//             console.log(day, "inchi Yanvar");
+//             break;
+//         case 2:
+//             console.log(day, "inchi Fevral");
+//             break;
+//         case 3:
+//             console.log(day, "inchi Mart");
+//             break;
+//         case 4:
+//             console.log(day, "inchi Aprel");
+//             break;
+//         case 5:
+//             console.log(day, "inchi May");
+//             break;
+//         case 6:
+//             console.log(day, "inchi Iyun");
+//             break;
+//         case 7:
+//             console.log(day, "inchi Iyul");
+//             break;
+//         case 8:
+//             console.log(day, "inchi Avgust");
+//             break;
+//         case 9:
+//             console.log(day, "inchi Sentabr");
+//             break;
+//         case 10:
+//             console.log(day, "inchi Oktabr");
+//             break;
+//         case 11:
+//             console.log(day, "inchi Noyabr");
+//             break;
+//         case 12:
+//             console.log(day, "inchi Dekabr");
+//             break;
+//     }
+// }else if(30 < day && 31>=day, 0 < month && 12>=month){
+//     switch(month){
+//         case 1:
+//             console.log(day, "inchi Yanvar");
+//             break;
+//         case 3:
+//             console.log(day, "inchi Mart");
+//             break;
+//         case 5:
+//             console.log(day, "inchi May");
+//             break;
+//         case 7:
+//             console.log(day, "inchi Iyul");
+//             break;
+//         case 8:
+//             console.log(day, "inchi Avgust");
+//             break;
+//         case 10:
+//             console.log(day, "inchi Oktabr");
+//             break;
+//         case 12:
+//             console.log(day, "inchi Dekabr");
+//             break;
+//     }
+// }else if(0 < day && 30>=day, 0 < month && 12>=month){
+//     switch(month){
+//         case 1:
+//             console.log(day, "inchi Yanvar");
+//             break;
+//         case 3:
+//             console.log(day, "inchi Mart");
+//             break;
+//         case 4:
+//             console.log(day, "inchi Aprel");
+//             break;
+//         case 5:
+//             console.log(day, "inchi May");
+//             break;
+//         case 6:
+//             console.log(day, "inchi Iyun");
+//             break;
+//         case 7:
+//             console.log(day, "inchi Iyul");
+//             break;
+//         case 8:
+//             console.log(day, "inchi Avgust");
+//             break;
+//         case 9:
+//             console.log(day, "inchi Sentabr");
+//             break;
+//         case 10:
+//             console.log(day, "inchi Oktabr");
+//             break;
+//         case 11:
+//             console.log(day, "inchi Noyabr");
+//             break;
+//         case 12:
+//             console.log(day, "inchi Dekabr");
+//             break;
+//     }
+// }else{
+//     console.log();
+// }
+
+// CASE-10
+
+// let y = "q", k = "0", location = y + k
+
+// switch(location){
+//     case j0:
+//         console.log("Janub");
+//         break;
+//     case j1:
+//         console.log("Sharq");
+//         break;
+//     case j2:
+//         console.log("G`arb");
+//         break;
+//     case g0:
+//         console.log("G`arb");
+//         break;
+//     case g1:
+//         console.log("Janub");
+//         break;
+//     case g2:
+//         console.log("Shimol");
+//         break;
+//     case s0:
+//         console.log("Shimol");
+//         break;
+//     case s1:
+//         console.log("G`arb");
+//         break;
+//     case s2:
+//         console.log("Sharq");
+//         break;
+//     case q0:
+//         console.log("Sharq");
+//         break;
+//     case q1:
+//         console.log("Shimol");
+//         break;
+//     case q2:
+//         console.log("Janub");
+//         break;
+    
+// }
+
+// CASE-11
+
+// let y = "q", k1 = "0", k2 = "1", location = y + k1 + k2
+
+// switch(location){
+//     case j00:
+//         console.log("Janub");
+//         break;
+//     case j01:
+//         console.log("G`arb");
+//         break;
+//     case j02:
+//         console.log("Sharq");
+//         break;
+//     case j11:
+//         console.log("Shimol");
+//         break;
+//     case j12:
+//         console.log("Janub");
+//         break;
+//     case j22:
+//         console.log("Shimol");
+//     case g00:
+//         console.log("G`arb");
+//         break;
+//     case g01:
+//         console.log("Shimol");
+//         break;
+//     case g02:
+//         console.log("Janub");
+//         break;
+//     case g11:
+//         console.log("Sharq");
+//         break;
+//     case g12:
+//         console.log("G`arb");
+//         break;
+//     case g22:
+//         console.log("Sharq");
+//     case s00:
+//         console.log("Shimol");
+//         break;
+//     case s01:
+//         console.log("Sharq");
+//         break;
+//     case s02:
+//         console.log("G`arb");
+//         break;
+//     case s11:
+//         console.log("Janub");
+//         break;
+//     case s12:
+//         console.log("Shimol");
+//         break;
+//     case s22:
+//         console.log("Janub");
+//     case q00:
+//         console.log("Sharq");
+//         break;
+//     case q01:
+//         console.log("Janub");
+//         break;
+//     case q02:
+//         console.log("Shimol");
+//         break;
+//     case q11:
+//         console.log("G`arb");
+//         break;
+//     case q12:
+//         console.log("Sharq");
+//         break;
+//     case q22:
+//         console.log("G`arb");
+// }
+
+// CASE-12  
+
+// const PI = 3.14
+// let  l = 50,r ,d, s;
+
+// if(!r && d){
+//      r = d/2
+// }else if(!r && s){
+//      r = Math.sqrt(s/PI)
+// }else if(!r && l){
+//      r = l/(2* PI)
+// }
+
+// let variable = l;
+
+// switch(variable){
+//     case r: 
+//         console.log(`D = ${r * 2 }`);
+//         console.log(`L = ${2* PI * r}`);
+//         console.log(`S = ${PI * Math.pow(r,2)}`);
+//         break;
+//     case d: 
+//         console.log(`R = ${d /2}`);
+//         console.log(`L = ${2* PI * r}`);
+//         console.log(`S = ${PI * Math.pow(r,2)}`);
+//         break;
+//     case s: 
+//         console.log(`R = ${r}`);
+//         console.log(`L = ${2* PI * r}`);
+//         console.log(`D = ${r * 2}`);
+//         break;
+//     case l:
+//         console.log(`R = ${r}`);
+//         console.log(`S = ${PI * Math.pow(r,2)}`);
+//         console.log(`D = ${r * 2}`);    
+// }
+
+// CASE-13
+
+// let s = 3 , c , h, a;
+
+// if(!a && c){
+//     a = c / Math.sqrt(2) 
+// }else if(!a && h){
+//     a = (2*h) / Math.sqrt(2)
+// }else if(!a && s){
+//     a = Math.sqrt(s * 2)
+// }
+
+// let variable = s
+
+// switch(variable){
+//     case a:
+//         console.log(`c = ${a * Math.sqrt(2)}`);
+//         console.log(`h = ${(a * Math.sqrt(2))/2}`);
+//         console.log(`S = ${(a * Math.sqrt(2)*(a * Math.sqrt(2)/2))/2}`);
+//         break;
+//     case c:
+//         console.log(`a = ${a}`);
+//         console.log(`h = ${(a * Math.sqrt(2))/2}`);
+//         console.log(`S = ${(a * Math.sqrt(2)*(a * Math.sqrt(2)/2))/2}`);
+//         break;
+//     case h:
+//         console.log(`c = ${a * Math.sqrt(2)}`);
+//         console.log(`a = ${a}`);
+//         console.log(`S = ${(a * Math.sqrt(2)*(a * Math.sqrt(2)/2))/2}`);
+//         break;
+//     case s:
+//         console.log(`c = ${a * Math.sqrt(2)}`);
+//         console.log(`h = ${(a * Math.sqrt(2))/2}`);
+//         console.log(`a = ${a}`);
+//         break;
+// }
+
+// CASE-14
+
+// let s = 3 , r1 , r2 , a;
+
+// if(!a && r1){
+//     a = (6*a)/Math.sqrt(3)
+// }else if(!a && r2){
+//     a = r2 * Math.sqrt(3)
+// }else if(!a && s){
+//     a = Math.sqrt((4*s)/Math.sqrt(3))
+// }
+
+// let variable = s
+
+// switch(variable){
+//     case a:
+//         console.log(`R1 = ${(a * Math.sqrt(3))/6}`);
+//         console.log(`R2 = ${a/Math.sqrt(3)}`);
+//         console.log(`S = ${(Math.pow(a,2)*Math.sqrt(3))/4}`);
+//     case r1:
+//         console.log(`a = ${a}`);
+//         console.log(`R2 = ${a/Math.sqrt(3)}`);
+//         console.log(`S = ${(Math.pow(a,2)*Math.sqrt(3))/4}`);
+//     case r2:
+//         console.log(`R1 = ${(a * Math.sqrt(3))/6}`);
+//         console.log(`a = ${a}`);
+//         console.log(`S = ${(Math.pow(a,2)*Math.sqrt(3))/4}`);
+//     case s:
+//         console.log(`R1 = ${(a * Math.sqrt(3))/6}`);
+//         console.log(`R2 = ${a/Math.sqrt(3)}`);
+//         console.log(`a = ${a}`);
+// }
+
+// CASE-15
